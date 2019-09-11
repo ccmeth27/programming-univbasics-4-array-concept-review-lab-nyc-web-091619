ee0471f076e7592aa433fd2092d7c011e81c219b
Connor Meth <connormeth27@gmail.com>
2019-09-11 17:30:41 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,6 +5,7 @@ valley = [5,4,3,2,1,0,1,2,3,4,5]
 zig_zag = [500,4,1000,5,250]
 
 def find_element_index(array, value_to_find)
+  array.length.times do 
     p array.map{|e| a.find_index(e)}
   	#	p array.find_index
   end
