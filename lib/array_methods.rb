98f77dc77142bcd7626ea8f08a7f6225f8f59eab
Connor Meth <connormeth27@gmail.com>
2019-09-11 17:37:38 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,8 +6,7 @@ zig_zag = [500,4,1000,5,250]
 
 def find_element_index(array, value_to_find)
   array.each do 
-    p array.map{|e| array.find_index(e)}
-  	array.find_index
+    p array.map{|value_to_find| array.find_index(value_to_find)}
   end
 end
 # find_element_index(scale, 2)
