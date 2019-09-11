c998666849216c269709dfa614916e1622bbbf7f
Connor Meth <connormeth27@gmail.com>
2019-09-11 17:26:00 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,22 @@
+scale = [2,3,4,5]
+scale_2 = [6,5,4,3]
+hill = [1,2,3,4,5,4,3,2,1]
+valley = [5,4,3,2,1,0,1,2,3,4,5]
+zig_zag = [500,4,1000,5,250]
+
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.length.times do 
+    p array.map{|e| a.find_index(e)}
+  	#	p array.find_index
+  end
 end
+# find_element_index(scale, 2)
+# find_element_index(scale, 4)
+# find_element_index(scale_2, 5)
+# find_element_index(scale_2, 6)
+# find_element_index(hill,500)
+# find_element_index(valley,500)
+# find_element_index(zig_zag,400)
 
 def find_max_value(array)
   # Add your solution here
